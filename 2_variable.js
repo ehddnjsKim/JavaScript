42c343ad189fce6a1d8f2736aa2239158946c704
김동원 <gimdongwon14@gmail.com>
2025-04-09 16:18:49 +0900

    JavaScript_1

---
+++ 2_variable.js
@@ -0,0 +1,9 @@
+let name; // 변수 선언, 값 : undefined
+console.log(name);
+name = "김사과"; // 갑 대입
+console.log(name);
+
+// const age; //값을 대입해야 함
+const age = 20;
+console.log(age);
+// age = 25 // 재대입 하지 못함
